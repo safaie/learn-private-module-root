bab375c2ce6c219b17ee7a8eb61140af8cf6d380
Ali Safaie <mohammadali.safaie@eficode.com>
2022-04-06 11:59:41 +0200

    updating main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/safaie/s3-webapp/aws"
+  version = "1.0.0"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
 }
